7c53dbe9be366ce8bf27d508080b4c8a2984a1d7
TanmayAjeetKumar <97678096+TanmayAjeetKumar@users.noreply.github.com>
2022-01-13 20:44:35 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,14 @@
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		StringBuilder s=new StringBuilder("Tanmay");
+		System.out.println(s);
+		
+		s.append("Works in mphasis");
+		System.out.println(s);
+	}
+
+}
