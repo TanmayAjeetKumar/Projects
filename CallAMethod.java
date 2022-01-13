99dac26be9c738cfe62a3dd3acf23346e472ed66
TanmayAjeetKumar <97678096+TanmayAjeetKumar@users.noreply.github.com>
2022-01-13 19:26:24 +0530

    Add files via upload

---
+++ CallAMethod.java
@@ -0,0 +1,13 @@
+
+public class CallAMethod {
+	void myOwnMEthod() {
+		System.out.println("You have called me! My name is: myMethod");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new CallAMethod().myOwnMEthod();
+
+	}
+
+}
