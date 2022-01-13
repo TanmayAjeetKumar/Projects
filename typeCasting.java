6f0bc165c5da016fadc99088c21e09ec2f8c83ae
TanmayAjeetKumar <97678096+TanmayAjeetKumar@users.noreply.github.com>
2022-01-13 19:00:20 +0530

    Add files via upload

---
+++ typeCasting.java
@@ -0,0 +1,33 @@
+
+public class typeCasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("implicit Type Casting");
+		char a='A';
+		System.out.println(a);
+		
+		int b=a;
+		System.out.println(b);
+		
+		float c=a;
+		System.out.println(c);
+		
+		long d=a;
+		System.out.println(d);
+		
+		double e=a;
+		System.out.println(e);
+		
+		System.out.println("\n");
+		
+		System.out.println("Explicit type casting");
+		
+		double x=45.5;
+		int y=(int)x;
+		System.out.println(x);
+		System.out.println(y);
+	}
+
+}
