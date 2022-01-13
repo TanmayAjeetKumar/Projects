a0107cc3ec9eb2e81c9bfb1161d40fb9bfef83de
TanmayAjeetKumar <97678096+TanmayAjeetKumar@users.noreply.github.com>
2022-01-13 19:10:15 +0530

    Add files via upload

---
+++ PrivateAccessModifiers.java
@@ -0,0 +1,12 @@
+
+public class PrivateAccessModifiers {
+	private String name;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Data d=new Data();
+		d.name="tanmay";
+
+	}
+
+}
