6155dd400f92e2e0ef5cfd2da089abd4b48246df
TanmayAjeetKumar <97678096+TanmayAjeetKumar@users.noreply.github.com>
2022-01-13 20:07:49 +0530

    Add files via upload

---
+++ Strings.java
@@ -0,0 +1,12 @@
+class Strings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String greet="Tanmay";
+		System.out.println("string: " +greet);
+		
+		int length=greet.length();
+		System.out.println("length: "+ length);
+	}
+
+}
