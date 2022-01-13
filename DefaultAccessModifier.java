c35a66cd87932f8a732f44652ca48c1ff7502c53
TanmayAjeetKumar <97678096+TanmayAjeetKumar@users.noreply.github.com>
2022-01-13 19:08:33 +0530

    Add files via upload

---
+++ DefaultAccessModifier.java
@@ -0,0 +1,13 @@
+
+public class DefaultAccessModifier {
+	void message() {
+		System.out.println("Its a message");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DefaultAccessModifier dam=new DefaultAccessModifier();
+		dam.message();
+	}
+
+}
